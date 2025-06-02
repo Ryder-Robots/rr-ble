f852dbe640c134e0a63dd58d0544b1c0df824e57
Aaron Spiteri <azzmosphere@gmail.com>
2025-06-02 18:29:40 +1000

    Refactor main.cpp to include rrobot.hpp and implement Serial data reading in loop function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
-#include <Arduino.h>
+#include <rrobot.hpp>
 
+using namespace rrobot;
 
 void setup() {
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+      uint8_t buf[BUFSIZ];
+      Serial.readBytesUntil(_TERM_CHAR, buf, BUFSIZ);
+
+      // check last char is _TERM_CHAR
+
+      String s = String(reinterpret_cast<char*>(buf));
+      rrevent e = rringress::deserialize(s);
+  }
 }
 
 #ifdef NATIVE
